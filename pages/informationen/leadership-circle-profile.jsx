dc9371bd0f669f4bd6916a07dcb40ea21a6f89a3
Dirk Dresch <dirk.dresch@gmail.com>
2022-11-27 21:12:20 +0100

    Fix text content

--- pages/informationen/leadership-circle-profile.jsx
+++ pages/informationen/leadership-circle-profile.jsx
@@ -13,13 +13,13 @@ export default function Page(props) {
             <div className='spacer-top'></div>
             <Headline line1="leadership circle profile&trade;" line2="eine 360° sicht auf sich" />
             <p>              
-              Leadership Circle Profile&trade; - LCP ist die führende 360 Grad Führungsstil Analyse. Das LCP ist ein Stärken basiertes 360-Modell und weist den Führungsstil in einer komplexen Mischung von Führungskompetenzen und inneren Überzeugungen aus. Dies unterstützt beim Auf- und Ausbau von Führungskompetenzen, -qualität und -effizienz. Jede der Ebenen enthält eine beachtliche Menge an Informationen und geht auch über Ihre Führungskompetenzen hinaus.
+              Leadership Circle Profile&trade; - LCP ist die führende 360 Grad Führungsstil Analyse. Das LCP ist ein Stärken basiertes 360-Modell und weist den Führungsstil in einer komplexen Mischung von Führungskompetenzen und inneren Überzeugungen aus. Dies unterstützt beim Auf- und Ausbau von Führungskompetenzen, -qualität und -effizienz. Jede der Ebenen enthält eine beachtliche Menge an Informationen und geht auch über die reinen Führungskompetenzen hinaus.
             </p>
             <p>
               <img src="/logo-lcp.png" alt="Leadership Circle Profile Logo" className='content-img-center' />
               Durch die visuelle Integration in Form einer kreisförmigen Grafik können bestimmte Muster und Schwerpunkte auf Anhieb erkannt werden. Die meisten anderen Instrumente zeigen die Kompetenzen als einfache Liste an und basieren auf reinen Verhaltensfragen.
             </p>
-            <Quote text={"Jede der Ebenen enthält eine beachtliche Menge an Informationen und geht auch über Ihre Führungskompetenzen hinaus."} />
+            <Quote text={"Jede der Ebenen enthält eine beachtliche Menge an Informationen und geht auch über die reinen Führungskompetenzen hinaus."} />
             <p>Das LCP integriert die Analyse reaktiver Führungsstile, kreativer Führungskompetenzen und schaut in die zugrundeliegenden Verhaltensmuster und Annahmen, die im aktuellen Kontext hervorkommen.</p>
             <p>Angesichts dessen ist das LCP eine aussagekräftige Beurteilung und Basis für die Weiterentwicklung der eigenen Stärken und der Führungseffektivität. Gleichzeitig werden die zugrunde liegenden Muster bewusst, die die Führungseffektivität verringern können. All dies wird unter Berücksichtigung der jeweiligen Entwicklungsstufe der Führungskräfte durchgeführt.</p>
             <img src="/lcp-example.png" alt="Leadership Circle Profile Beispiel" className='content-img-center' />
