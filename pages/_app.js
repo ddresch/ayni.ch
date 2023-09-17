7db23eda49dbeca9ef69de1f7dfd1de5490393f5
Dirk Dresch <dirk.dresch@gmail.com>
2023-09-17 16:44:20 +0200

    Add canonical link

--- pages/_app.js
+++ pages/_app.js
@@ -16,8 +16,13 @@ function AppLayout({ Component, pageProps, router }) {
     <>
       <Head>
           <title>ayni.ch - Deine Praxis f&uuml;r Ayurveda Ern&auml;hrungs- und Massagetherapie in Basel</title>          
+          
           <meta name="description" content="Willkommen bei ayni, Ihrer Praxis für Ayurveda Ernährungs- und Massagetherapie, Beratung und Coaching. Wir bieten individuelle Lösungen für ein gesundes und energievolles Leben, Stressbewältigung, persönliches Wachstum und Corporate Wellbeing. Unser Leitsatz: 'Gib Dir zurück, damit Du anderen geben kannst.'" />
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+          <meta name="theme-color" content="#ffffff" />
 
+          <link rel="canonical" href={`https://ayni.ch${router.asPath}`} />
           <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
           <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
           <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
@@ -31,10 +36,7 @@ function AppLayout({ Component, pageProps, router }) {
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-          <link rel="manifest" href="/manifest.json" />
-          <meta name="msapplication-TileColor" content="#ffffff" />
-          <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-          <meta name="theme-color" content="#ffffff" />
+          <link rel="manifest" href="/manifest.json" />          
       </Head>
 
       <Header />
