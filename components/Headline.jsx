8e229756a5c29d15acaf6fcd3cc5c1d234f12732
Dirk Dresch <dirk.dresch@gmail.com>
2023-09-17 21:18:01 +0200

    Format lines

--- components/Headline.jsx
+++ components/Headline.jsx
@@ -1,27 +1,23 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from 'react'
+import PropTypes from 'prop-types'
 import styles from '../styles/Headline.module.css'
 
 export const Headline = ({ line1, line2, sublevel, ...props }) => {
-
-  console.log('Headline: sublevel: ', sublevel)
-
-    return (
-        (sublevel)
-        ? <h2 className={styles.headline}>{line1} <span>{line2}</span></h2> 
-        : <h1 className={styles.headline}>{line1} <span>{line2}</span></h1>
-    )
-  }
+  return (
+      (sublevel)
+      ? <h2 className={styles.headline}>{line1} <span>{line2}</span></h2> 
+      : <h1 className={styles.headline}>{line1} <span>{line2}</span></h1>
+  )
+}
   
-  Headline.propTypes = {
-    line1: PropTypes.string,    
-    line2: PropTypes.string,
-    sublevel: PropTypes.bool,
-  }
-  
-  Headline.defaultProps = {
-    line1: 'Headline',
-    line2: 'Subline',
-    sublevel: true,
-  }
-    
+Headline.propTypes = {
+  line1: PropTypes.string,    
+  line2: PropTypes.string,
+  sublevel: PropTypes.bool,
+}
+
+Headline.defaultProps = {
+  line1: 'Headline',
+  line2: 'Subline',
+  sublevel: true,
+}
