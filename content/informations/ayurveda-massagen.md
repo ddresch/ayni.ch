6bb9f39697a2cb1526436f0e61d6d9c46b81f50a
Dirk Dresch <dirk.dresch@gmail.com>
2024-06-30 20:23:20 +0200

    Add CMS to informations section

---
+++ content/informations/ayurveda-massagen.md
@@ -0,0 +1,32 @@
+---
+title: Ayurveda Massagen
+subtitle: übersicht behandlungen
+publishedAt: 2023-02-01T14:00:32.566Z
+---
+# Allgemeine Informationen
+
+Die Ayurveda-Massagen werden mit speziellen Kräuter-Ölen durchgeführt, die in den meisten Fällen auch nach der Behandlung auf der Haut verbleiben können. Bitte ältere, gut waschbare und bequeme Kleidung mitbringen oder anziehen. Bei einigen Behandlungen werden auch die Haare eingeölt, bitte eine Kopfbedeckung wie Mütze oder einen Schal mitbringen, um den Kopf vor Wind und Kälte zu schützen.
+
+Einige Ayurveda-Massagen sind während der Schwangerschaft oder bei den Mens kontraindiziert. In diesem Fall den Termin bitte bis 48 Stunden vorher verschieben oder ich passe die Art der Massage / Behandlung an.
+
+# Ayurveda Massagen
+
+## Teilkörpermassagen
+
+* <b>Padabhyanga</b><br/>eine kühlende, entspannende und vitalisierende Fussmassage mit Ghee
+* <b>Pristabhyanga</b><br/>eine lösende und entspannende Rückenmassage für geschmeidigere Bewegung
+* <b>Mukabhyanga</b><br/>diese Kopf- und Gesichtsmassage ist ein himmlisch entspannendes Erlebnis, welche den Geist beruhigt
+* <b>Shiroabhyanga</b><br/>eine Kopf, Nacken, Schulter Massage um etwaige Verspannungen in diesem Bereich zu lösen
+
+## Ganzkörpermassagen
+
+* <b>Abhyanga</b><br/>die Einölung mit den liebenden Händen Ganzkörper-Ölmassage sorgt für Verjüngung und Beruhigung von Körper und Geist
+* <b>Udvartana</b><br/>die Pulvermassage aktiviert den Stoffwechsel, das Lymphsystem und macht die Haut durch den Peeling effekt weich und zart
+* <b>Gharshana</b><br/>die trockene Seidenhandschuh-Massage hilft der Aktivierung des Lymphsystems und der Sinne
+* <b>Kalari</b><br/>ürsprünglich aus der Südindischen Kampfkunst entsanden öffnet diese Massagen Blockaden, verbessert die Beweglichkeit, reinigt, kräftigt und harmoinisiert den Organismus
+* <b>7-Stellungsmassage</b><br/>die Ganzkörpereinölung in 7 Stellungen wirkt ausgleichend und vitalisierend
+
+## Spezialmassagen
+
+* <b>Basti</b><br/>die lokalen Ölanwendung auf Gelenken und Rücken sorgen für Nährung und Entspannung
+* <b>Upanasveda</b><br/>diese Tiefengewebe-Marmamassage auf dem Rücken wirkt besonders lösend durch spezifische Öle, Kräuter und Wärme Anwendung
---
+++ pages/informationen/[slug].jsx
@@ -0,0 +1,65 @@
+import fs from 'fs'
+import * as React from 'react'
+import matter from 'gray-matter'
+import yaml from 'js-yaml'
+import { Headline } from '../../components/Headline'
+import Layout from '../../components/Layout'
+import Head from 'next/head'
+import Markdown from 'react-markdown'
+
+export default function Post({ frontmatter, markdown}) {
+  return (<>
+      <Head>
+        <title>CMS Page: {frontmatter.title}</title>
+        <meta name="description" content="Entdecken Sie ayni balance für ganzheitliche Lösungen bei stressbedingten Beschwerden und Burnout in Basel. Wir bieten individuelle Therapiepläne, Stressbewältigungsstrategien, Achtsamkeitstraining und Ayurvedische Massagen. Erstgespräch ab 150 CHF. Krankenkassen-akzeptiert. Finden Sie Ihre innere Balance heute." />
+        <meta name="keywords" content="ayni balance, Stressbewältigung, Burnout, Resilienz, Achtsamkeit, Mentaltraining, Stressmanagement" />
+      </Head>
+      <Layout>
+        <div className='main-container'>
+          <div className='spacer-top'></div>
+          <Headline line1={frontmatter.title}
+                    line2={frontmatter.subtitle}
+                    sublevel={false}
+          />
+          <div className='markdown-body'>
+            <Markdown>{markdown}</Markdown>
+          </div>
+        </div>
+        <div className='spacer-top'></div>
+      </Layout>
+  </>)
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  const fileContent = matter(fs.readFileSync(`./content/informations/${slug}.md`, 'utf8'), {
+    engines: {yaml: (s) => yaml.load(s, { schema: yaml.JSON_SCHEMA })}
+  })
+  let frontmatter = fileContent.data
+  const markdown = fileContent.content
+
+  return {
+    props: { frontmatter, markdown }
+  }
+}
+  
+export async function getStaticPaths() {
+  const filesInProjects = fs.readdirSync('./content/posts')
+
+  // Getting the filenames excluding .md extension
+  // and returning an array containing slug (the filename) as params for every route
+  // It looks like this
+  // paths = [
+  //   { params: { slug: 'my-first-blog' }},
+  //   { params: { slug: 'how-to-train-a-dragon' }},
+  //   { params: { slug: 'how-to-catch-a-pokemon' }},
+  // ]
+  const paths = filesInProjects.map(file => {
+    const filename = file.slice(0, file.indexOf('.'))
+    return { params: { slug: filename }}
+  })
+
+  return {
+    paths,
+    fallback: false // This shows a 404 page if the page is not found
+  }
+}
