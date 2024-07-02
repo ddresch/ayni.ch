c27ae04120bb560c10feecd52ddcd0c1c21a05af
Dirk Dresch <dirk.dresch@gmail.com>
2024-07-02 23:24:39 +0200

    Add testimonials to CMS

---
+++ content/allgemein/testimonials.md
@@ -0,0 +1,12 @@
+---
+title: Testimonials
+subtitle: ayni ganz ehrlich
+seoTitle: title: Testimonials ehrliche Stimmen über ayni in Basel
+seoDescription: Erfahren Sie mehr über Ayurveda, das Wissen vom Leben, bei ayni
+  in Basel. Entdecken Sie die Grundlagen der 3 Doshas - Vata, Pitta, Kapha - und
+  die Gunas für mentale Balance. Ziel des Ayurveda ist Svastha, das Verweilen im
+  Selbst, durch die richtige Balance von Körper, Geist und Seele.
+seoKeywords: Ayurveda, Doshas, Vata, Pitta, Kapha, Gunas, Tamas, Rajas, Sattva,
+  Lebensphilosophie, Gesundheit
+publishedAt: 2023-02-01T14:00:32.566Z
+---
--- pages/testimonials.jsx
+++ pages/testimonials.jsx
@@ -1,15 +1,33 @@
+import fs from 'fs'
 import * as React from 'react'
-import { ContentContainer } from '../components/ContentContainer'
+import matter from 'gray-matter'
+import yaml from 'js-yaml'
+import Head from 'next/head'
+import Markdown from 'react-markdown'
 import { Headline } from '../components/Headline'
 import Layout from '../components/Layout'
-import { Quote } from '../components/Quote'
+import { useEffect } from 'react'
 
-export default function Page(props) {
-  return (
+export default function Page({ frontmatter, markdown}) {
+
+  useEffect(() => {
+    console.log('Testimonials useEffect', frontmatter, markdown)
+  }, [])
+
+  return (<>
+    <Head>
+      <title>{frontmatter.seoTitle}</title>
+      <meta name="description" content={frontmatter.seoDescription} />
+      <meta name="keywords" content={frontmatter.seoKeywords} />
+    </Head>
     <Layout>
         <div className='main-container'>
             <div className='spacer-top'></div>
-            <Headline line1="Testimonials" line2="ayni ganz ehrlich" sublevel={false} />
+            <Headline 
+              line1={frontmatter.title}
+              line2={frontmatter.subtitle}
+              sublevel={false}
+            />
             <h2>May, 45, The Netherlands</h2>
             <p>Silvia is an excellent health and wellbeing coach, tuned to both, to the wide range of resources of traditional Ayurvedic medicine, as well as to the very modern conditions in which we find ourselves. She has excellent attention to detail as well as genuine warmth, compassion and many other admirable human qualities.</p>
             <p>Thanks to Silvia&apos;s thorough and thoughtful consultation, she was able to recommend to me a range of measures to change my diet, daily routine, and daily practices, for enormously positive results. Changes in diet, eating habits and daily routines—along with the integration of a number of Ayurvedic daily health practices—have significantly improved/increased my energy, balance, outlook on life, and physical, mental and spiritual well-being.</p>
@@ -28,5 +46,21 @@ export default function Page(props) {
         </div>        
         <div className='spacer-top'></div>
     </Layout>
-  )
+  </>)
+}
+
+
+export async function getStaticProps({ params: { slug } }) {
+  const fileContent = matter(fs.readFileSync(`./content/allgemeines/${slug}.md`, 'utf8'), {
+    engines: {yaml: (s) => yaml.load(s, { schema: yaml.JSON_SCHEMA })}
+  })
+  let frontmatter = fileContent.data
+  const markdown = fileContent.content
+
+  return {
+    props: { 
+      frontmatter, 
+      markdown 
+    }
+  }
 }
