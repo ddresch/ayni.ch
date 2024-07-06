d910e804916037be2f3661f44d1b3e9c6370e48c
Dirk Dresch <dirk.dresch@gmail.com>
2024-07-06 12:38:02 +0200

    Add startpage to CMS

---
+++ content/allgemein/startseite.md
@@ -0,0 +1,58 @@
+---
+title: Silvia Ferlito
+subtitle: ayurveda, beratung & coaching
+seoTitle: Testimonials ehrliche Stimmen über ayni in Basel
+seoDescription: Erfahren Sie mehr über Ayurveda, das Wissen vom Leben, bei ayni
+  in Basel. Entdecken Sie die Grundlagen der 3 Doshas - Vata, Pitta, Kapha - und
+  die Gunas für mentale Balance. Ziel des Ayurveda ist Svastha, das Verweilen im
+  Selbst, durch die richtige Balance von Körper, Geist und Seele.
+seoKeywords: Ayurveda, Doshas, Vata, Pitta, Kapha, Gunas, Tamas, Rajas, Sattva,
+  Lebensphilosophie, Gesundheit
+publishedAt: 2023-02-01T14:00:32.566Z
+description: >-
+  Willkommen bei ayni - Praxis für Ayurveda Ernährungs- und Massagetherapie, Beratung und Coaching in Basel.
+
+  ![Portrait Silvia Ferlito](images/portrait-wall.png)
+
+  Das Wort ayni kommt aus den Anden und bedeutet auf Quechua Schenken. Damit wird die gegenseitige Hilfe der Mitglieder einer Gemeinschaft beschrieben. Ayni bezeichnet somit die Gegenseitigkeit und Wechselbeziehung zwischen dem Innen und Aussen, dem Nehmen und Geben.
+
+
+  > Gib Dir zurück damit Du anderen geben kannst.
+
+
+  Durch dieses universelle Gesetz von Gleichgewicht im Universum, dem ständigen Austausch zwischen Mensch, Natur und Universum auch auf energetischer Ebene ist das eine im Anderen vorhanden.
+
+  
+  So verhält sich es auch im Ayurveda. Das Universum als Makrokosmos und der Mensch als Mikrokosmos widerspiegeln sich in einem direkten Zusammenhang und stehen in Wechselbeziehung zu sich.
+
+  
+  Diese beiden Konzepte vereine ich in meiner Arbeit mit dem Leitsatz *„Gib Dir zurück, damit Du anderen geben kannst.“* für ein Gleichgewicht zwischen Mensch, Natur und Universum und für den Einklang zwischen Körper, Geist und Seele.
+
+
+  Mit meiner Arbeit verbinde ich generelle Lebensthemen, persönliches Wachstum, Leadership Entwicklung, Ernährung, Lifestyle und Wellbeing und biete eine gezielte individuelle Begleitung an.
+
+
+  Deine Silvia Ferlito
+offers:
+  - title: ayni wellbeing
+    description: >-
+      Individuelle Ayurveda Ernährungs-, Gesundheitsberatung und Massagetherapie für ein gesundes und energievolles Leben
+    link: angebot/wellbeing
+  - title: ayni balance
+    description: >-
+      Ganzheitliche Lösungswege für stressbedingte Beschwerden, Burnout, Resilienz und Mentaltraining
+    link: angebot/balance
+  - title: ayni beratung
+    description: >-
+      Coaching, Mentaltraining und Psychologische Beratung des Ayurveda lebendig, praxisnah und alltagsgerecht
+    link: angebot/beratung
+  - title: ayni leadership
+    description: >-
+      Unterstützung bei Persönlichkeits-, Karriereentwicklung mit der LCP 360 Grad Bewertung und bei der Bewältgung von Lebenskrisen
+    link: angebot/leadership
+  - title: ayni corporate
+    description: >-
+      Ganzheitliche Lösungswege für ein Nachhaltiges Corporate & Employee Wellbeing
+    link: angebot/corporate
+
+---
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,17 @@
-import Link from 'next/link'
 import * as React from 'react'
+import Head from 'next/head'
+import fs from 'fs'
+import matter from 'gray-matter'
+import yaml from 'js-yaml'
+import Markdown from 'react-markdown'
+import Link from 'next/link'
+// Custom
 import ContactForm from '../components/ContactForm'
 import { ContentContainer } from '../components/ContentContainer'
 import { Headline } from '../components/Headline'
 import Layout from '../components/Layout'
-import { Quote } from '../components/Quote'
 
-export default function LandingPage(props) {
+export default function LandingPage({frontmatter, markdown}) {
     React.useEffect(() => {
         // This will run only once when the app component is mounted
         if (window.netlifyIdentity) {
@@ -20,72 +25,58 @@ export default function LandingPage(props) {
         }
     }, [])
 
-  return (
-    <Layout>
-        <div className='main-container'>
+    return (<>
+        <Head>
+            <title>{frontmatter.seoTitle}</title>
+            <meta name="description" content={frontmatter.seoDescription} />
+            <meta name="keywords" content={frontmatter.seoKeywords} />
+        </Head>
+        <Layout>
+            <div className='main-container'>
+                <div className='spacer-top'></div>
+                <Headline 
+                    line1={frontmatter.title}
+                    line2={frontmatter.subtitle}
+                    sublevel={false}
+                />
+                <Markdown>
+                    {frontmatter.description}
+                </Markdown>
+                <div className='spacer-md'></div>
+                <Headline line1="Mein Angebot umfasst" line2="" />
+                <ul>
+                    {frontmatter.offers.map((item, index) => (
+                        <li key={index}>
+                            <b>{item.title}:</b>&nbsp;
+                            {item.description}
+                            <Link href={item.link} className='more-link'>mehr erfahren</Link>
+                        </li>
+                    ))}
+                </ul>
+                <div className='spacer-md'></div>
+            </div>
+
+            <ContentContainer primaryColor={'#faedcd'}>
+                <Headline line1="Termin anfragen" line2="zeit f&uuml;r dich" />
+                <p>Du m&ouml;chtest einen Termin anfragen? Einfach das untere Formular ausf&uuml;llen und ich melde mich direkt bei Dir!</p>            
+                <ContactForm />
+            </ContentContainer>                   
             <div className='spacer-top'></div>
-            <Headline line1="Silvia Ferlito" line2="ayurveda, beratung &amp; coaching" sublevel={false} />
-            <p>
-                Willkommen bei ayni - Praxis für Ayurveda Ernährungs- und Massagetherapie, Beratung und Coaching in Basel.
-            </p>
-            <p>
-                <img src="portrait-wall.png" className='content-img-right' alt="Portrait Silvia Ferlito" />
-                Das Wort ayni kommt aus den Anden und bedeutet auf Quechua Schenken. Damit wird die gegenseitige Hilfe der Mitglieder einer Gemeinschaft beschrieben. Ayni bezeichnet somit die Gegenseitigkeit und Wechselbeziehung zwischen dem Innen und Aussen, dem Nehmen und Geben.
-            </p>
-            <Quote text="Gib Dir zurück damit Du anderen geben kannst."/>
-            <p>                
-                Durch dieses universelle Gesetz von Gleichgewicht im Universum, dem ständigen Austausch zwischen Mensch, Natur und Universum auch auf energetischer Ebene ist das eine im Anderen vorhanden. 
-            </p>
-            <p>
-                So verhält sich es auch im Ayurveda. Das Universum als Makrokosmos und der Mensch als Mikrokosmos widerspiegeln sich in einem direkten Zusammenhang und stehen in Wechselbeziehung zu sich. 
-            </p>
-            <p>
-                Diese beiden Konzepte vereine ich in meiner Arbeit mit dem Leitsatz <i>„Gib Dir zurück, damit Du anderen geben kannst.“</i> für ein Gleichgewicht zwischen Mensch, Natur und Universum und für den Einklang zwischen Körper, Geist und Seele.
-            </p>
-            <p>
-                Mit meiner Arbeit verbinde ich generelle Lebensthemen, persönliches Wachstum, Leadership Entwicklung, Ernährung, Lifestyle und Wellbeing und biete eine gezielte individuelle Begleitung an.
-            </p>
-            <p>
-                Deine Silvia Ferlito
-            </p>
+        </Layout>
+    </>)
+}
 
-            <div className='spacer-md'></div>
-            <Headline line1="Mein Angebot umfasst" line2="" />
-            <ul>
-                <li>
-                    <b>ayni wellbeing:</b>&nbsp;
-                    Individuelle Ayurveda Ernährungs-, Gesundheitsberatung und Massagetherapie für ein gesundes und energievolles Leben
-                    <Link href="angebot/wellbeing" className='more-link'>mehr erfahren</Link>
-                </li>
-                <li>
-                    <b>ayni balance:</b>&nbsp; 
-                    Ganzheitliche Lösungswege für stressbedingte Beschwerden, Burnout, Resilienz und Mentaltraining
-                    <Link href="angebot/balance" className='more-link'>mehr erfahren</Link>
-                </li>
-                <li>
-                    <b>ayni beratung:</b>&nbsp; 
-                    Coaching, Mentaltraining und Psychologische Beratung des Ayurveda lebendig, praxisnah und alltagsgerecht
-                    <Link href="angebot/beratung" className='more-link'>mehr erfahren</Link>
-                </li>
-                <li>
-                    <b>ayni leadership:</b>&nbsp; 
-                    Unterstützung bei Persönlichkeits-, Karriereentwicklung mit der LCP 360 Grad Bewertung und bei der Bewältgung von Lebenskrisen
-                    <Link href="angebot/leadership" className='more-link'>mehr erfahren</Link>
-                </li>
-                <li>
-                    <b>ayni corporate:</b>&nbsp; 
-                    Ganzheitliche Lösungswege für ein Nachhaltiges Corporate &amp; Employee Wellbeing
-                    <Link href="angebot/corporate" className='more-link'>mehr erfahren</Link>
-                </li>
-            </ul>
-        </div>
-
-        <ContentContainer primaryColor={'#faedcd'}>
-            <Headline line1="Termin anfragen" line2="zeit f&uuml;r dich" />
-            <p>Du m&ouml;chtest einen Termin anfragen? Einfach das untere Formular ausf&uuml;llen und ich melde mich direkt bei Dir!</p>            
-            <ContactForm />
-        </ContentContainer>       
-        <div className='spacer-top'></div>         
-    </Layout>
-  )
+export async function getStaticProps() {
+    const fileContent = matter(fs.readFileSync(`./content/allgemein/startseite.md`, 'utf8'), {
+      engines: {yaml: (s) => yaml.load(s, { schema: yaml.JSON_SCHEMA })}
+    })
+    let frontmatter = fileContent.data
+    const markdown = fileContent.content
+  
+    return {
+      props: { 
+        frontmatter, 
+        markdown 
+      }
+    }
 }
