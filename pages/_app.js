9c415c1ba0dfca1c5541212ab1d88d2543de282b
Dirk Dresch <ddresch@users.noreply.github.com>
2025-06-25 10:16:49 +0200

    Update _app.js
    Remove HTML comment of google tag

--- pages/_app.js
+++ pages/_app.js
@@ -38,8 +38,7 @@ function AppLayout({ Component, pageProps, router }) {
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-          <link rel="manifest" href="/manifest.json" />  
-          <!-- Google tag (gtag.js) -->
+          <link rel="manifest" href="/manifest.json" />
           <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17188148066"></script>
           <script>
             window.dataLayer = window.dataLayer || [];
